2f7a9c95d4baf73ef4dbe708bf1aebcb3f2f209b
afmahego <afmahechag@gmail.com>
2023-11-16 23:06:09 -0500

    punto2

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 <img src="./public/images/punto-1.png" alt="punto 1">
 
 <h2>Punto 2 : Sumas de Constantes y Variables</h2>
+<img src="./public/images/punto-2.png" alt="punto 2">
 
 <h2>Punto 3 : Suma de dos números dados  por el usuario</h2>
 
---
+++ punto-2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+const FIRSTNUMBER = 15;
+$secondNumber = 25;
+$suma = FIRSTNUMBER + $secondNumber;
+ 
+echo "La suma de"." " . FIRSTNUMBER ." ". "y" . " ". "$secondNumber"." " ."es"." ". $suma . "\n";
+
+$secondNumber = 100;
+$suma = FIRSTNUMBER + $secondNumber;
+echo "La suma de"." " . FIRSTNUMBER ." ". "y" . " ". "$secondNumber"." " ."es"." ". $suma . "\n";
+
+?>
