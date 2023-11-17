f7b797f4adc922514b416b038c5722d643bb8266
afmahego <afmahechag@gmail.com>
2023-11-16 22:48:24 -0500

    punto 1

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 
 <h2>Punto 1 : Constantes y Variables</h2>
+<img src="./public/images/punto-1.png" alt="punto 1">
 
 <h2>Punto 2 : Sumas de Constantes y Variables</h2>
 
---
+++ punto-1/index.php
@@ -0,0 +1,12 @@
+<?php
+
+const SALUDO = "Buenos Días";
+$name ="Felipe";
+$lastName ="Mahecha";
+echo SALUDO. "\n"." $name "." "."$lastName " . "\n" ;
+
+$name ="Pepito";
+$lastName="Pérez";
+echo SALUDO."\n"." $name "." "."$lastName "; 
+
+?>
