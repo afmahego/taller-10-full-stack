f0507a4291a180e997525b1c48645a3dd120b4d1
afmahego <afmahechag@gmail.com>
2023-11-24 21:53:43 -0500

    punto15

--- punto-15/index.php
+++ punto-15/index.php
@@ -15,46 +15,4 @@ echo " El resultado de la operación indicada es : " . $result ;
 
 ?>
 
-<?php
-function getNumber(){
-  echo " Ingrese número a operar \n";
-  return readline(); 
-}
-function showMenu (){
-    echo"Dígite operacion a realizar \n";
-    echo "1 - SUMA \n ";
-    echo "2 - RESTA \n ";
-    echo "3 - MULTIPLICACION \n ";
-    echo "4 - DIVISION \n ";
-  return readline(); 
-}
-function operate ($x, $y , $option){
-  switch($option) {
-  case 1: 
-        return add($x ,$y);
-  case 2 :
-        return sub($x,$y);
-  case 3: 
-      return dot($x ,$y);
-  case 4 :
-      return div($x,$y);
-    default:
-    echo"La opción indicada no es válida" ;
-    return 0;
-}
-  return ;
-}
-function add($x,$y){
-  return $x + $y;
-}
-function sub($x,$y){
-  return $x - $y;
-}
-function dot ($x, $y){
-  return $x * $y;
-}
-function div ($x,$y){
-  return $x/$y;
-}
-?>
 
